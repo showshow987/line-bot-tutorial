0ae07a393ca0079f1542447ccd9889757d1239c6
showshow <showshow987@gmail.com>
2017-05-08 21:24:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,65 +36,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 picture = ["https://i.imgur.com/qKkE2bj.jpg",
            "https://i.imgur.com/QjMLPmx.jpg",
            "https://i.imgur.com/HefBo5o.jpg",
-           "https://i.imgur.com/AjxWcuY.jpg",
-           "https://i.imgur.com/3vDRl4r.jpg",
-           "https://i.imgur.com/3qSGcKT.jpg",
-           "https://i.imgur.com/ZbdV9Nz.jpg",
-           "https://i.imgur.com/oAkIJmH.jpg",
-           "https://i.imgur.com/MtcwDtD.jpg",
-           "https://i.imgur.com/qre60t1.jpg",
-           "https://i.imgur.com/Yrvc7LV.jpg",
-           "https://i.imgur.com/4wJXl4D.jpg",
-           "https://i.imgur.com/71suURR.jpg",
-           "https://i.imgur.com/sNBVjhg.jpg",
-           "https://i.imgur.com/h5HJmGx.jpg",
-           "https://i.imgur.com/O92zfAa.jpg",
-           "https://i.imgur.com/eaQyCc9.jpg",
-           "https://i.imgur.com/CEuYLJ6.jpg",
-           "https://i.imgur.com/yD8RcYu.jpg",
-           "https://i.imgur.com/cOLTxKC.jpg",
-           "https://i.imgur.com/pYQHJXU.jpg",
-           "https://i.imgur.com/JC68vsX.jpg",
-           "https://i.imgur.com/4hEWo2f.jpg",
-           "https://i.imgur.com/FW6wzFO.jpg",
-           "https://i.imgur.com/pgMFTp1.jpg",
-           "https://i.imgur.com/GWoZrQB.jpg",
-           "https://i.imgur.com/ytByPTQ.jpg",
-           "https://i.imgur.com/Qta7jlq.jpg",
-           "https://i.imgur.com/PByM0FF.jpg",
-           "https://i.imgur.com/xCLD2QP.jpg",
-           "https://i.imgur.com/vq7ONzd.jpg",
-           "https://i.imgur.com/OKtXWJS.jpg",
-           "https://i.imgur.com/RonVK6S.jpg",
-           "https://i.imgur.com/cH9oLjI.jpg",
-           "https://i.imgur.com/sn4p43t.jpg",
-           "https://i.imgur.com/LaKmM7c.jpg",
-           "https://i.imgur.com/7YzFhNt.jpg",
-           "https://i.imgur.com/O6j2qDB.jpg",
-           "https://i.imgur.com/N4pkG9S.jpg",
-           "https://i.imgur.com/1SlHQU6.jpg",
-           "https://i.imgur.com/mplQ8IO.jpg",
-           "https://i.imgur.com/tO1R8Xt.jpg",
-           "https://i.imgur.com/nCgWLuY.jpg",
-           "https://i.imgur.com/ZQfoFsa.jpg",
-           "https://i.imgur.com/ApmQia8.jpg",
-           "https://i.imgur.com/CiUyuZb.jpg",
-           "https://i.imgur.com/hfhA6d4.jpg",
-           "https://i.imgur.com/KOljinG.jpg",
-           "https://i.imgur.com/XmRwW0U.jpg",
-           "https://i.imgur.com/Ee8CFje.jpg",
-           "https://i.imgur.com/yNkxNkA.jpg",
-           "https://i.imgur.com/hnkzX6p.jpg",
-           "https://i.imgur.com/rrdr3zZ.jpg",
-           "https://i.imgur.com/hzbdQU9.jpg",
-           "https://i.imgur.com/xdNOHGc.jpg",
-           "https://i.imgur.com/b2B1LPE.jpg",
-           "https://i.imgur.com/BUfqlcN.jpg",
-           "https://i.imgur.com/8yl3W2D.jpg",
-           "https://i.imgur.com/DbxBheB.jpg",
-           "https://i.imgur.com/DDNc9ot.jpg",
-           "https://i.imgur.com/hh2e3LT.jpg",
-           "https://i.imgur.com/2cdURNa.jpg"
+           "https://i.imgur.com/AjxWcuY.jpg"
            ]
 
 
@@ -371,14 +313,7 @@ def getMenu():
         data = filename+'\n'
         content += data
     if os.path.isfile(file_path):
-        content += 'ooo'
-        with open("menu/requ33.txt", "r") as f:
-            while True:
-                line = f.readline().strip()
-                if not line:
-                    break
-                content += line
-            
+        content += 'ooo'            
     else:
         content += 'xxx'
     '''
@@ -397,14 +332,14 @@ def getMenu():
             #print(r.no, r.item, r.price)
             data = r.no +' '+ r.item +' '+ r.price + '\n'
             #content += data
-    
-    with open('123.txt', "r") as f:
+    '''    
+    with open("menu/requ33.txt", "r") as f:
         while True:
-            line = f.readline().strip()
+            line = f.readline()
             if not line:
                 break
             content += line
-    '''
+    content += '\n請輸入號碼點餐:'
     return content
 
 @handler.add(MessageEvent, message=TextMessage)
